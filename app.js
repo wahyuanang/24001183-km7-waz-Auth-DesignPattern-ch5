c112fb998ff399f2e354f359456e0287233f382d
wahyuanang <wahyuanng@students.amikom.ac.id>
2024-11-04 23:05:11 +0700

    fix: swagger

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const docsRouter = require("./routes/documentationRoute");
 
 const router = require("./routes");
 const app = express();
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(morgan("dev"));
+app.use("/api-docs", docsRouter)
 app.use("/api/v1", router);
 
 module.exports = app;
